885885c981ab12bbd14cae914ecb866c6fbe3ad7
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-03-05 17:09:38 -0500

    update state, give loading msg

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,8 @@
-import /* we need our action types here*/ "../actions";
+import { FETCH_CHARS, FETCH_CHARS_FAILURE, FETCH_CHARS_SUCCESS } from '../actions';
 const initialState = {
-  characters: []
+  characters: [],
+  fetching: false,
+  error: null
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +10,12 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCH_CHARS:
+      return { ...state, fetching: true};
+    case FETCH_CHARS_SUCCESS:
+      return { ...state, characters: action.payload.results, fetching: false };
+    case FETCH_CHARS_FAILURE:
+      return { ...state, fetching: false, error: action.payload };
     default:
       return state;
   }
